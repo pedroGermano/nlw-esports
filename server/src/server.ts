f56a5096f6e1213b4022029732cd419b63f5198a
Pedro Germano <pedrogermano232@gmail.com>
2022-09-16 18:42:03 -0300

    Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,26 @@
-import express from 'express';
+import express, { response } from "express";
 
-const app  = express();
+const app = express();
 
-app.get('/ads', (request, response) => {
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+   return response.status(201).json([]);
+});
+
+
+app.get("/games/:id/ads", (request, response) => {
   return response.json([
-    { id: 1, name: 'Anúcio 1'},
-    { id: 2, name: 'Anúcio 2'},
-    { id: 3, name: 'Anúcio 3'}
-  ])
+    { id: 1, name: "Anúcio 1" },
+    { id: 2, name: "Anúcio 2" },
+    { id: 3, name: "Anúcio 3" },
+  ]);
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  return response.json([]);
 })
 
 app.listen(3333);
